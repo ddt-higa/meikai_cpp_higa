2e06311626d691ef538b53a0185bb4ff6064d256
ddt-higa <ddt.higa@gmail.com>
2019-03-07 21:31:50 +0900

    第1章 演習問題1_1-1_5

---
+++ 第1章 画面への出力とキーボードからの入力/問題1-4/問題1-4/問題1-4.cpp
@@ -0,0 +1,12 @@
+//演習1-4 1行に1文字ずつ自分の名前を表示するプログラムを作成せよ。
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//自分の名前を一文字ずつ表示します
+	cout << "比\n";
+	cout << "嘉\n";
+	cout << "一\n";
+	cout << "晃\n";
+}
